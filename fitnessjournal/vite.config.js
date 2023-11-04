bd6f7006070d79760b13f1b5571462a686ad6876
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-11-04 16:52:09 -0500

    Added some comments for future features

--- fitnessjournal/vite.config.js
+++ fitnessjournal/vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// Notes:
+// instead of creating multiple nested routes to create exercises/add exercises,
+// I'll restucture like this:
+// - main dashboard/new workout screen with start new workout/add exercises/previous workouts,
+// - create new exercise page
+// - create new category page
+// 
