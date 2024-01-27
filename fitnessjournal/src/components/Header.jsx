8632bba9aa38b84b3e596c86dbf222099c73c74d
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-01-26 20:33:37 -0600

    Deleted ExerciseFiles.jsx

--- fitnessjournal/src/components/Header.jsx
+++ fitnessjournal/src/components/Header.jsx
@@ -3,57 +3,72 @@ import { Link } from "react-router-dom"
 import { logout } from "../firebase"
 
 export default function Header() {
-    // const [openMenu, setOpenMenu] = useState(false)
-    // const [openAddMenu, setOpenAddMenu] = useState(false)
-    // const [openModal, setOpenModal] = useState(false)
-    // const menuRef = useRef(null)
-    // const addMenuRef = useRef(null)
+    const [openMenu, setOpenMenu] = useState(false)
+    const [openAddMenu, setOpenAddMenu] = useState(false)
+    const [openModal, setOpenModal] = useState(false)
+    const menuRef = useRef(null)
+    const addMenuRef = useRef(null)
 
-    // function signOutUser() {
-    //     logout()
-    //     window.location.reload()
-    // }
+    function signOutUser() {
+        logout()
+        window.location.reload()
+    }
 
     // const addMenuStyles = {
     //     height: openAddMenu ? "160px" : "0",
     //     transition: openAddMenu ? ".2s ease-in-out" : ""
     // }
 
-    // const settingsMenuStyles = {
-    //     height: openMenu ? "265px" : "0",
-    //     transition: openMenu ? ".2s ease-in-out" : ""
-    // }
+    const settingsMenuStyles = {
+        height: openMenu ? "265px" : "0",
+        transition: openMenu ? ".2s ease-in-out" : ""
+    }
 
     // const ellipsisStyles = {
     //     transform: openMenu ? "rotate(90deg)" : "rotate(0)"
     // }
 
-    // function toggleMenu(e) {
-    //     if(e.target.dataset.settings) {
-    //         setOpenMenu(prev => !prev)
-    //     } else if(e.target.dataset.add) {
-    //         setOpenAddMenu(prev => !prev)
-    //     }
+    function toggleMenu(e) {
+        if(e.target.dataset.settings) {
+            setOpenMenu(prev => !prev)
+        } else if(e.target.dataset.add) {
+            setOpenAddMenu(prev => !prev)
+        }
         
-    // }
+    }
 
-    // const handleClickOutside = (e) => {
-    //     if(menuRef.current && !menuRef.current.contains(e.target)) {
-    //         setOpenMenu(false)
-    //     }
-    //     if(addMenuRef.current && !addMenuRef.current.contains(e.target)) {
-    //         setOpenAddMenu(false)
-    //     }
-    // }
+    const handleClickOutside = (e) => {
+        if(menuRef.current && !menuRef.current.contains(e.target)) {
+            setOpenMenu(false)
+        }
+        if(addMenuRef.current && !addMenuRef.current.contains(e.target)) {
+            setOpenAddMenu(false)
+        }
+    }
 
-    // document.addEventListener("click", handleClickOutside)
+    document.addEventListener("click", handleClickOutside)
 
     return (
         <header>
             <h2 className="app-title">The Fitbook</h2>
-            <span class="material-symbols-outlined account-icon">
+            <span ref={menuRef} class="material-symbols-outlined account-icon" data-settings="settings" onClick={(e) => toggleMenu(e)}>
                 account_circle
             </span>
+                <ul className="user-menu" style={settingsMenuStyles}>
+                    <Link to="/" className="menu-item-link">
+                        <li className="menu-item">Dashboard</li>
+                    </Link>
+                    <Link className="menu-item-link">
+                        <li className="menu-item">Settings</li>
+                    </Link>
+                    <Link className="menu-item-link">
+                        <li className="menu-item">Log Analysis</li>
+                    </Link>
+                    <Link className="menu-item-link">
+                        <li className="menu-item">Body Stats</li>
+                    </Link>
+                    <li onClick={signOutUser} className="menu-item">Logout</li>
+                </ul>
             {/* <nav className="nav">
                 <i ref={addMenuRef} className="fa-solid fa-plus nav-item" data-add="add" onClick={(e) => toggleMenu(e)}></i>
                 <ul className="user-menu" style={addMenuStyles}>
--- fitnessjournal/src/server.js
+++ fitnessjournal/src/server.js
@@ -1,15 +1,27 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require("express")
 
-const app = express();
-app.use(express.json());
+const app = express()
 
-mongoose.connect('mongodb://localhost:27017/exercise')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+app.listen(3000, () => {
+    console.log(" server running on port 3000 http://localhost:3000 ")
+})
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+
+// const app = express();
+// app.use(express.json());
+
+// mongoose.connect('mongodb://localhost:27017/exercise')
+//     .then(() => console.log('Connected to MongoDB...'))
+//     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // import express from "express"
 // import mongoose from "mongoose"
