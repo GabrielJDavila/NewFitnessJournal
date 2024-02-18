8f40ee6d182dc455db1ac2dfaab3ae8f7e250f27
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-02-17 19:01:19 -0600

    Update to SignUp.jsx
    Created more inputs to create user profile. Will be an optional feature.

--- fitnessjournal/src/components/SignUp.jsx
+++ fitnessjournal/src/components/SignUp.jsx
@@ -8,6 +8,7 @@ export default function SignUp(props) {
                 <h1 className="login-form-title">Sign Up New User</h1>
                 <p onClick={e => props.flipShowLogin(e)} className="switch-to-login">Already have an account? Log in here.</p>
                 <fieldset className="login-input-fieldset">
+                    <label>Login Info</label>
                     <input
                         name="email"
                         type="email"
@@ -30,6 +31,78 @@ export default function SignUp(props) {
                         required
                     />
                 </fieldset>
+
+                <fieldset className="login-input-fieldset">
+                    <label>Create Profile</label>
+                    <p>(Optional)</p>
+                    <input
+                        name="name"
+                        type="text"
+                        data-signup="signup"
+                        onChange={e => props.handleChange(e)}
+                        value={props.name}
+                        placeholder="Your name"
+                        className="login-cred"
+                        required
+                    />
+                    
+                    <input
+                        name="age"
+                        type="text"
+                        data-signup="signup"
+                        onChange={e => props.handleChange(e)}
+                        value={props.password}
+                        placeholder="age"
+                        className="login-cred"
+                        required
+                    />
+                    <div className="weight-and-height-container">
+                        <input
+                            name="weight"
+                            type="text"
+                            data-signup="signup"
+                            onChange={e => props.handleChange(e)}
+                            value={props.password}
+                            placeholder="weight"
+                            className="login-cred"
+                            required
+                        />
+                        <div className="height-container">
+                            <input
+                                name="feet-height"
+                                type="text"
+                                data-signup="signup"
+                                onChange={e => props.handleChange(e)}
+                                value={props.password}
+                                placeholder="ft"
+                                className="login-cred"
+                                required
+                            />
+                            <input
+                                name="feet-inches"
+                                type="text"
+                                data-signup="signup"
+                                onChange={e => props.handleChange(e)}
+                                value={props.password}
+                                placeholder="in"
+                                className="login-cred"
+                                required
+                            />
+                        </div>
+                    </div>
+                    <input
+                        name="gender"
+                        type="text"
+                        data-signup="signup"
+                        onChange={e => props.handleChange(e)}
+                        value={props.password}
+                        placeholder="gender"
+                        className="login-cred"
+                        required
+                    />
+                    
+                </fieldset>
+                
                 <button data-signup="true" className="login-button">Login</button>
             </form>
         </div>
