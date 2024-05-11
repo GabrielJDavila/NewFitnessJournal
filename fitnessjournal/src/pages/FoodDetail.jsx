721403a66a5fd5c93371bdbcae027b1e4ffa8394
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-05-10 22:20:21 -0500

    Update to FoodDetail.jsx / index.css
    Successfully rendering nutrient details on page. Added some styles to page.

--- fitnessjournal/src/pages/FoodDetail.jsx
+++ fitnessjournal/src/pages/FoodDetail.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-
+import BackBtn from "../components/BackBtn"
 export default function FoodDetail() {
     const params = useParams()
     const foodId = params.id
     const [foodData, setFoodData] = useState(null)
-   console.log(foodData)
+    const firstChar = foodData ? foodData.description.charAt(0).toUpperCase() : ""
+    const slicedLetters = foodData ? foodData.description.toLowerCase().slice(1) : ""
+    const capWord = foodData && firstChar + slicedLetters
+    console.log(foodData)
     useEffect(() => {
         fetchedSingleFoodData()
     }, [])
@@ -29,9 +32,46 @@ export default function FoodDetail() {
         
     }
 
+    // const nutrients = []
+    // for(const key in foodData.labelNutrients) {
+    //     if(foodData.labelNutrients.hasOwnProperty(key)) {
+    //         const nutrient = foodData.labelNutrients[key]
+    //         nutrients.push(
+    //             <div></div>
+    //         )
+    //     }
+    // }
+
+    // const nutrientExample = foodData && foodData.labelNutrients ? Object.entries(foodData.labelNutrients) : ""
+    // console.log(foodData.labelNutrients)
+
+    const nutrients = foodData && Object.entries(foodData.labelNutrients).map(([key, nutrient], index) => {
+        return (
+            <div key={index} className="nutrient-container">
+                <p>{key}</p>
+                <p>{Math.round(nutrient.value)}</p>
+            </div>
+        )
+    })
+    console.log(nutrients)
+
+    // const nutrients = foodData & foodData.labelNutrients.map((nutrient, index) => {
+    //     return (
+    //         <div key={index} className="nutrient-container">
+    //             {nutrient.value}
+    //         </div>
+    //     )
+    // })
+
     return (
         <div className="food-detail-page">
-            <h1>Food Detail Page</h1>
+            <div className="food-detail-top-container">
+                <BackBtn />
+                <p>{capWord}</p>
+            </div>
+            <div className="all-nutrients-container">
+                {nutrients}
+            </div>
         </div>
     )
 }
