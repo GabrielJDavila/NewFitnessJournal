de5ea063232d6696dcd79d467fda0861c91d3154
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-09-17 22:03:51 -0500

    Update CurrentWorkoutList.jsx
    Added some state to control the planned "edit/view" modal of each exercise. Now just to create the pop-up component.

--- fitnessjournal/src/components/CurrentWorkoutList.jsx
+++ fitnessjournal/src/components/CurrentWorkoutList.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
 import { Link, useOutletContext } from "react-router-dom"
 import { Draggable } from "react-beautiful-dnd"
 export default function CurrentWorkoutList(props) {
-   
+    const [flipView, setFlipView] = useState(new Array(props.data.length).fill(false))
+    console.log(props.data)
+    console.log(flipView)
+
+    // function to reset flipView to close any existing edit/view modals
+    function resetFlipView() {
+        setFlipView(new Array(flipView.length).fill(false))
+    }
+
+    // function to flip current flipView boolean to true. Allows for edit/view modal to pop up for given exercise.
+    function handleFlipView(e) {
+        resetFlipView()
+        console.log(e.target.dataset.flipview)
+        const index = e.target.dataset.flipview
+        setFlipView(prev => {
+            const boolArr = [...prev]
+            boolArr[index] = !boolArr[index]
+            return boolArr
+        })
+    }
+
         const currentWorkout = props.data.map((ex, index) => {
             return (
                 <Draggable key={ex.id} draggableId={ex.id} index={index}>
@@ -18,11 +39,14 @@ export default function CurrentWorkoutList(props) {
                             <div className="ex-name-container">
                                 <p className="ex-number">{index + 1}</p>
                                 <p className="current-ex-name">{ex.name}</p>
-                                <i
-                                    onClick={e => props.toggleDel(e)}
-                                    data-deleteexid={ex.id}
-                                    className="fa-solid fa-trash curr-ex-delete"
-                                ></i>
+                                <i onClick={e => handleFlipView(e)} data-flipview={index} className="fa-solid fa-ellipsis-vertical"></i>
+                                <div className="ex-detail-div">
+                                    <i
+                                        onClick={e => props.toggleDel(e)}
+                                        data-deleteexid={ex.id}
+                                        className="fa-solid fa-trash curr-ex-delete"
+                                    ></i>
+                                </div>
                             </div>
                             
                             <ul className="all-sets-container">
