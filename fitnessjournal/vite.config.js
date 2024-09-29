8b4dd0d94f89c9a4cd731edf6e152dda6f872e71
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-09-29 14:44:17 -0500

    Set up Vite-PWA plugin

--- fitnessjournal/vite.config.js
+++ fitnessjournal/vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'The Fitbook',
+        short_name: 'TFB',
+        start_url: '/',
+        icons: [
+          {
+            src: '/assets/TheFitbook.png',
+            sizes: '500x500',
+            type: 'image/png',
+          },
+        ],
+        background_color: '#c65d42',
+        display: 'standalone',
+        theme_color: '#c65d42',
+      },
+      devOptions: {
+        enabled: true,
+      }
+    }),
+  ],
 })
 
 // Notes:
