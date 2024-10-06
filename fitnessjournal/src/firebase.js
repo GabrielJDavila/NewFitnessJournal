d9f1c8a56c2f225927cbd480a296ecb3354b2321
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-10-06 18:28:20 -0500

    edit to env files

--- fitnessjournal/src/firebase.js
+++ fitnessjournal/src/firebase.js
@@ -35,14 +35,26 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+<<<<<<< Updated upstream
 connectFirestoreEmulator(db, '127.0.0.1', 8080)
+=======
+>>>>>>> Stashed changes
 const auth = getAuth()
-connectAuthEmulator(auth, "http://127.0.0.1:9099");
 
+<<<<<<< Updated upstream
 if(process.env.NODE_ENV === "development") {
     connectFirestoreEmulator(db, '127.0.0.1', 8080)
     connectAuthEmulator(auth, "http://127.0.0.1:9099");
 }
+=======
+// connectFirestoreEmulator(db, '127.0.0.1', 8081)
+// connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+// if(import.meta.env.VITE_NODE_ENV === "development") {
+//     connectFirestoreEmulator(db, '127.0.0.1', 8081)
+//     connectAuthEmulator(auth, "http://127.0.0.1:9099");
+// }
+>>>>>>> Stashed changes
 
 export { auth }
 
