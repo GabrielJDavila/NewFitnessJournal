9f094d51ffd545afa3a9d21e10852c6739913b73
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2024-10-06 18:35:44 -0500

    Test change

--- fitnessjournal/dev-dist/sw.js
+++ fitnessjournal/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dtf4hopc6fo"
+    "revision": "0.htfked5q5l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
